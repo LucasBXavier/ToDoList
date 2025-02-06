fbad30aa1fcdcc8d90964c9c3b4e7e036a7516fc
LucasBXavier <lucas_b.xavier@hotmail.com>
2025-02-05 21:57:59 -0300

    innit commit

---
+++ src/main/java/com/lucasboareto/todolist/user/UserController.java
@@ -0,0 +1,22 @@
+package com.lucasboareto.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
+    }
+
+
+
+}
---
+++ src/main/java/com/lucasboareto/todolist/user/UserModel.java
@@ -0,0 +1,25 @@
+package com.lucasboareto.todolist.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name ="tb_users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    private String userName;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
